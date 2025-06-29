02b6155818b31985d7fd4aba2fa206f04747e413
Melisa Cihan <melisa-cihan@outlook.de>
2025-06-29 15:30:14 +0200

    apply column splitting heuristics in normalizer class

--- src/main/java/Normalizer.java
+++ src/main/java/Normalizer.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedHashMap; // Explicitly used for row maps
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import normalizer.heuristics.HeuristicRule;
